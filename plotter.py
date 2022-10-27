abd09e995d94f9acf9499b9d7ae1bd9f1b73847f
Alex Moini <alexandermoini@gmail.com>
2022-10-27 13:33:31 -0600

    formatting fixes

--- plotter.py
+++ plotter.py
@@ -1,13 +1,24 @@
+from this import d
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import argparse
 
+def convert_df_vals_to_float(dataframe, column_names):
+    """
+    Name: convert_df_vals_to_float
+    Parameters: dataframe, column_names
+    Returns: dataframe
+    """
+    for column_name in column_names:
+        dataframe[column_name] = dataframe[column_name].astype(float)
+    return dataframe
+
 def make_box_plot(dataframe, column_names, y_label, title):
     """
-    Name:
-    Parameters:
-    Returns:
+    Name: make_box_plot
+    Parameters: dataframe, column_names, y_label, title
+    Returns: 0
     """
     plt.boxplot(dataframe[column_names],labels=column_names)
     plt.ylabel(y_label)
@@ -18,9 +29,9 @@ def make_box_plot(dataframe, column_names, y_label, title):
 
 def make_scatter_plot(dataframe, x_label, y_label, title):
     """
-    Name:
-    Parameters:
-    Returns:
+    Name: make_scatter_plot
+    Parameters: dataframe, x_label, y_label, title
+    Returns: 0
     """
     for species_name in set(dataframe[title]):
         iris_subset = dataframe[dataframe[title] == species_name]
@@ -39,12 +50,13 @@ def make_multi_panel_figure(suptitle, dataframe, column_names,
                             x_labels, y_labels, title_scatter,
                             title_box):
     """
-    Name:
-    Parameters:
-    Returns:
+    Name: make_multi_panel_figure
+    Parameters: suptitle, dataframe, column_names, x_labels, y_labels, title_scatter, title_box
+    Returns: 0
     """
     fig, axes = plt.subplots(1,2)
     fig.suptitle(suptitle)
+    # dont include header in df
     for species_name in set(dataframe[title_scatter]):
         iris_subset = dataframe[dataframe[title_scatter] == species_name]
         axes[0,0].scatter(iris_subset[x_labels[0]], iris_subset[y_labels[0]],
@@ -58,17 +70,22 @@ def make_multi_panel_figure(suptitle, dataframe, column_names,
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_name", help="the name of the file to read from (string)")
-    parser.add_argument("column_names", help="the name of the columns to plot (string)")
-    parser.add_argument("y_label_box", help="the name of the y label (string)")
-    parser.add_argument("title_box", help="the name of the title (string)")
-    parser.add_argument("x_label_scatter", help="the name of the x label (string)")
-    parser.add_argument("y_label_scatter", help="the name of the y label (string)")
-    parser.add_argument("title_scatter", help="the name of the title (string)")
-    parser.add_argument("suptitle", help="the name of the suptitle (string)")
+    parser.add_argument("--file_name", help="the name of the file to read from (string)")
+    parser.add_argument("--column_names", help="the name of the columns to plot (string)")
+    parser.add_argument("--y_label_box", help="the name of the y label (string)")
+    parser.add_argument("--title_box", help="the name of the title (string)")
+    parser.add_argument("--x_label_scatter", help="the name of the x label (string)")
+    parser.add_argument("--y_label_scatter", help="the name of the y label (string)")
+    parser.add_argument("--title_scatter", help="the name of the title (string)")
+    parser.add_argument("--suptitle", help="the name of the suptitle (string)")
     args = parser.parse_args()
     iris = pd.read_csv(args.file_name)
     column_names = args.column_names.split(',')
+    # add header to iris, col names
+    iris.columns = column_names
+    # convert values to float
+    float_cols = column_names[0:3]
+    iris = convert_df_vals_to_float(iris, float_cols)
     make_box_plot(iris, column_names, args.y_label_box, args.title_box)
     make_scatter_plot(iris, args.x_label_scatter,
                       args.y_label_scatter, args.title_scatter)
--- test_plotter.sh
+++ test_plotter.sh
@@ -3,6 +3,6 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 
 conda init --all
 
-conda activate assignment-7
+conda activate assignment-7-swe4s
 
 run test_for
